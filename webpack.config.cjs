2b34b67127738829b7b4f8ddd1d2578b2fcf6744
Javier Medina <130412345+engineer34@users.noreply.github.com>
2026-04-23 00:52:56 -0500

    Add files via upload

---
+++ webpack.config.cjs
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+
+  entry: "./src/index.js",
+
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+ devServer: {
+  static: {
+    directory: path.resolve(__dirname, "dist"),
+  },
+  hot: true,
+},
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
